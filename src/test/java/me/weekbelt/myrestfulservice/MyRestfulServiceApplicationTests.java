93ddbb293ea5b41839f39bbb367e5f02d90cc12a
weekbelt <vfrvfr4207@gmail.com>
2021-04-19 16:12:15 +0900

    Initial commit

---
+++ src/test/java/me/weekbelt/myrestfulservice/MyRestfulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.weekbelt.myrestfulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRestfulServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
