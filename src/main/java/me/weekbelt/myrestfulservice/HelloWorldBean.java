6b746a34cb1cd618d7d17428c53119e0ab81dfa3
weekbelt <vfrvfr4207@gmail.com>
2021-04-19 16:18:35 +0900

    HelloWorld Bean 추가

---
+++ src/main/java/me/weekbelt/myrestfulservice/HelloWorldBean.java
@@ -0,0 +1,14 @@
+package me.weekbelt.myrestfulservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class HelloWorldBean {
+
+    private String message;
+
+}
