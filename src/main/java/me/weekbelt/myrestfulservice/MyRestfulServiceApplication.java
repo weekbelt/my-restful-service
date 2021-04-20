47845accf3179b4ed2cf42eb0c3423f56722a921
weekbelt <vfrvfr4207@gmail.com>
2021-04-20 22:35:21 +0900

    다국어 처리를 위한 Internationalization 구현

--- src/main/java/me/weekbelt/myrestfulservice/MyRestfulServiceApplication.java
+++ src/main/java/me/weekbelt/myrestfulservice/MyRestfulServiceApplication.java
@@ -1,7 +1,11 @@
 package me.weekbelt.myrestfulservice;
 
+import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class MyRestfulServiceApplication {
@@ -10,4 +14,10 @@ public class MyRestfulServiceApplication {
         SpringApplication.run(MyRestfulServiceApplication.class, args);
     }
 
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.KOREA);
+        return localeResolver;
+    }
 }
