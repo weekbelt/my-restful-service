8b30c907da6c9028fe7e0f050d72aab94463464b
weekbelt <vfrvfr4207@gmail.com>
2021-04-24 23:50:45 +0900

    Swagger 적용

---
+++ src/main/java/me/weekbelt/myrestfulservice/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package me.weekbelt.myrestfulservice.config;
+
+import io.swagger.annotations.Api;
+import io.swagger.models.Contact;
+import java.util.ArrayList;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2);
+    }
+}
